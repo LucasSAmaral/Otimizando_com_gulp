0c8f0b3303c2430afbf6f2f58f267773f71db5f9
Lucas <lucassantosamaral@hotmail.com>
2018-11-07 17:49:48 -0200

    Otimizando bibliotecas

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,19 @@
 var gulp = require('gulp');
 var customizeBootstrap = require('gulp-customize-bootstrap');
 var less = require('gulp-less');
+var concat = require('gulp-concat');
 
-gulp.task('default',['compileBootstrap']);
+gulp.task('default',['compileBootstrap','lib']);
 
 gulp.task('compileBootstrap', function() {
-    return gulp.src('node_modules/bootstrap/less/bootstrap.less')
+    return gulp.src('./node_modules/bootstrap/less/bootstrap.less')
       .pipe(customizeBootstrap('./dev/styles/less/*.less'))
       .pipe(less())
       .pipe(gulp.dest('./dist/css/'));
-  });
+});
+
+gulp.task('lib', function(){
+    return gulp.src('./node_modules/jquery/dist/jquery.min.js')
+    .pipe(concat('lib.js'))
+    .pipe(gulp.dest('./dist/js/'));
+});
